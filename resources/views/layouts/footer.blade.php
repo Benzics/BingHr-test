6cd816fa56f3e4ed6153c84497a00021dccb7b1e
benzics <benjaminnicholas29@gmail.com>
2022-04-29 04:54:08 +0100

    First sidebar section design in progress

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,7 @@
-<!-- bootstrap js -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<!-- Jquery -->
+<!-- jquery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,12 +5,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bing Hr</title>
+    <title>BingHr</title>
+
 
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- our css -->
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
 </head>
 
 <body>
 
-    <h1>Hello User</h1>
+
+    <div class="row">
+        <aside class="col-lg-2">
+            <div class="row no-gutters full-height">
+                <div class="col-lg-3 nav-area">
+                    <ul class="first-nav">
+                        <a href="#">
+                            <li class="nav-item"><i class="fa-solid fa-magnifying-glass"></i></li>
+                        </a>
+                        <a href="#">
+                            <li class="nav-item"> <i class="fa-solid fa-table-columns"></i></li>
+                        </a>
+                        <a href="#">
+                            <li class="nav-item"> <i class="fa-solid fa-user"></i></li>
+                        </a>
+                        <a href="#">
+                            <li class="nav-item"> <i class="fa-solid fa-comment"></i></li>
+                        </a>
+                        <a href="#">
+                            <li class="nav-item"> <i class="fa-solid fa-file"></i></li>
+                        </a>
+
+
+
+                    </ul>
+                </div>
+                <div class="col-lg-9 bg-white">
+                    <nav>
+                        <ul class="nav flex-column sidebar">
+                            <li class="nav-item"><i class="fa-solid fa-table-columns"></i>Dashboard</li>
+                            <li class="nav-item">Test</li>
+                            <li class="nav-item">Test</li>
+                            <li class="nav-item">Test</li>
+                            <li class="nav-item">Test</li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </aside>
+        <div class="col-lg-10">
+            <div class="row">
+                <div class="col-lg-6">
+                    <span>Users</span>
+                    <form action="/" method="get">
+                        <select name="year" id="year">
+                            <option value="" disabled="" selected="">Year</option>
+                            <option value="2022">2022</option>
+                        </select>
+                        <input type="search" name="u" id="u" placeholder="Search..">
+                    </form>
+                </div>
+
+                <div class="col-lg-6">
+
+                </div>
+            </div>
+        </div>
+    </div>
