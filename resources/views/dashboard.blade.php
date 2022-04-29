5eb37e9b795f1f35e3b46a10d4d673ce15ffde40
benzics <benjaminnicholas29@gmail.com>
2022-04-29 23:39:57 +0100

    Table rows styling updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -130,6 +130,36 @@
                         <th scope="col">Action</th>
                     </tr>
                 </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="d-flex">
+                                <div class="mr-auto">
+                                    <div class="d-flex">
+                                        <img src="{{ asset('/assets/images/avatar.png')}}" alt="" class="avatar">
+                                        <div>
+                                            <div class="user-name">David Wagner</div>
+                                            <div class="user-email">david_wagner@example.com</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <span class="tags sa">
+                                        Super Admin
+                                    </span>
+                                </div>
+
+                            </div>
+                        </td>
+                        <td><span class="meta">24 Oct 2015</span></td>
+                        <td><span class="meta">CEO and Founder</span></td>
+                        <td>
+                            <a href="#" class="action-btn"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="#" class="action-btn"><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </section>
