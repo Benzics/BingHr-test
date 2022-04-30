684187a0af787a08ce99519e392cd676cf2ac706
benzics <benjaminnicholas29@gmail.com>
2022-04-30 01:22:53 +0100

    Add user form template complete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -271,10 +271,114 @@
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <div class="container">
+                    <form action="/" method="post" name="add_user" id="add_user">
+                        <div class="form-group">
+                            <input type="number" name="id" class="form-control" placeholder="Employee ID*" />
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" name="first_name" class="form-control" placeholder="First Name*">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="text" name="last_name" class="form-control" placeholder="Last Name*">
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <input type="email" name="email" class="form-control" placeholder="Email ID*">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <input type="tel" name="phone" class="form-control" placeholder="Mobile No">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <select name="role" id="role" class="form-control">
+                                        <option value="" selected="" disabled="">Select Role Type</option>
+                                        <option value="1">Admin</option>
+                                        <option value="0">Employee</option>
+                                        <option value="2">HR Admin</option>
+                                        <option value="3">Super Admin</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <input type="text" name="username" class="form-control" placeholder="Username*">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control" placeholder="Password*">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password*">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="container mt-5">
+                    <div class="table-responsive">
+                        <table class="table perm">
+                            <thead class="header-g">
+                                <tr>
+                                    <th>Module Permission</th>
+                                    <th>Read</th>
+                                    <th>Write</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Super Admin</td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Admin</td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-regular fa-square"></i></td>
+                                    <td><i class="fa-regular fa-square"></i></td>
+                                </tr>
+                                <tr>
+                                    <td>Employee</td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-regular fa-square"></i></td>
+                                    <td><i class="fa-regular fa-square"></i></td>
+                                </tr>
+                                <tr>
+                                    <td>HR Admin</td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                    <td><i class="fa-solid fa-square-check"></i></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Add User</button>
+                <button type="submit" form="add_user" class="btn btn-primary">Add User</button>
                 <button type="button" class="btn " data-dismiss="modal">Cancel</button>
 
             </div>
